d26557933cc089e229066827f007256a425fabb0
Antonio <iamdesvelao@gmail.com>
2024-02-17 11:31:14 +0100

    remove: remove deprecated commands

--- commands/dota2/patch.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'patch',
-  category : 'Dota 2',
-  help : 'Current Dota 2 patch',
-  args : '',
-  response: (msg, args, client, command) => client.cache.dota2Patch,
-}
--- commands/owner/_bot.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'bot',
-  category : 'Owner',
-  help : 'Base command to control the bot',
-  args : '<cmd>',
-  requirements: ['owner.only'],
-  run: async function(msg, args, client){
-  }
-}
