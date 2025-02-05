c5689e37b8d1ec793c8ad1b338fda92af88bb0ef
Antonio <iamdesvelao@gmail.com>
2025-02-05 19:20:50 +0100

    fix: tasks scheduled time

--- src/tasks/update-database-backup.js
+++ src/tasks/update-database-backup.js
@@ -1,6 +1,6 @@
 module.exports = function createTaskUpdateLeaderBoard(ctx) {
   return {
-    timer: '* * * * 0', // every sunday at 00:00
+    timer: '0 0 * * 0', // every sunday at 00:00
     execute: () => {
       if (ctx.client.config.switches.backupdb) {
         ctx.client.components.Bot.takeDatabaseBackup();
--- src/tasks/update-leaderboard.js
+++ src/tasks/update-leaderboard.js
@@ -1,6 +1,6 @@
 module.exports = function createTaskUpdateLeaderBoard(ctx) {
   return {
-    timer: '* * * * 0', // every sunday at 00:00
+    timer: '0 0 * * 0', // every sunday at 00:00
     execute: () => {
       if (ctx.client.config.switches.leaderboardUpdate) {
         ctx.client.components.Bot.updateLeaderboard();
